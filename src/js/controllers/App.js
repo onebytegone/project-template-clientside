e6af1dadbcce5a657da33f4b8292c1e2f2c4eacf
Ethan Smith <ethan@onebytegone.com>
2017-05-16 12:52:13 -0400

    Add jQuery usage test

--- src/js/controllers/App.js
+++ src/js/controllers/App.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var Class = require('class.extend');
+var $ = require('jquery'),
+    Class = require('class.extend');
 
 module.exports = Class.extend({
 
    sayHello: function() {
       console.log('Hello!');  // eslint-disable-line no-console
+      $('.app').append($('<h1>Hello</h1>'));
    },
 
 });
